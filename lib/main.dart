badf1f13da957deabad59a5dd44d735eebc90649
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-02-16 15:36:01 +0200

    app separated from the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,8 @@
 import 'package:flutter/material.dart';
+import 'app/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Quote Generator APP',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Text('Main Screen'),
-    );
-  }
+  runApp(
+    const QuoteGeneratorApp(),
+  );
 }
