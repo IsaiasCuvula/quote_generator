b39aabbfd9427ddee280ca870611977897556c4e
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-02-17 20:18:03 +0200

    route config class immutable

--- lib/routers/routes_config.dart
+++ lib/routers/routes_config.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:quote_generator/routers/routes.dart';
 
+@immutable
 class RoutesConfig {
   static final _navigationKey = GlobalKey<NavigatorState>();
   static final routeConfig = GoRouter(
