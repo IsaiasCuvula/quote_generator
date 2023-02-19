e44976f2681bc4715a1eb32c6a37cdfaf75a3c64
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-02-19 18:21:42 +0200

    app bar done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
+import 'package:quote_generator/theme/dimensions.dart';
 
 class HomeScreen extends StatelessWidget {
   static HomeScreen builder(
@@ -12,9 +14,23 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Explore'),
+        centerTitle: false,
+        title: Padding(
+          padding: Dimensions.kPaddingAllSmall,
+          child: Text(
+            'Explore',
+            style: theme.textTheme.titleLarge,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const FaIcon(FontAwesomeIcons.gear),
+          )
+        ],
       ),
     );
   }
