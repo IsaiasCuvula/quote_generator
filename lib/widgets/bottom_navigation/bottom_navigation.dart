fcb64d2dc1b89539acd35514d638573094103744
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-02-25 15:41:07 +0200

    improved

---
+++ lib/widgets/bottom_navigation/bottom_navigation.dart
@@ -0,0 +1,4 @@
+
+
+export 'bottom_nav_bar_item.dart';
+export 'bottom_navigation_bar.dart';
--- lib/widgets/bottom_navigation/bottom_navigation_bar.dart
+++ lib/widgets/bottom_navigation/bottom_navigation_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quote_generator/theme/dimensions.dart';
+import 'package:quote_generator/theme/theme.dart';
+import 'bottom_nav_bar_item.dart';
 
 class BottomNavBar extends StatelessWidget {
   const BottomNavBar({super.key, required this.items});
@@ -17,30 +18,6 @@ class BottomNavBar extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: items,
         ),
-      ),
-    );
-  }
-}
-
-class BottomNavBarItem extends StatelessWidget {
-  const BottomNavBarItem({
-    super.key,
-    required this.child,
-    required this.onTap,
-  });
-
-  final Widget child;
-  final VoidCallback onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkResponse(
-      onTap: onTap,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          child,
-        ],
       ),
     );
   }
