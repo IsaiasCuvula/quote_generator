5bc3bf4be5efceba0099c2fcfde60bc5d7255090
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-04-14 03:48:22 +0300

    SnackBar color onSurface for dark & light theme

--- lib/utils/helpers.dart
+++ lib/utils/helpers.dart
@@ -95,11 +95,14 @@ class Helpers {
   }
 
   static showSnackbar(BuildContext context, String message, bool isSucess) {
+    final theme = Theme.of(context);
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text(
           message,
-          style: Theme.of(context).textTheme.bodySmall,
+          style: theme.textTheme.bodySmall?.copyWith(
+            color: theme.colorScheme.onSurface,
+          ),
         ),
         backgroundColor: isSucess ? Colors.green : Colors.red,
       ),
