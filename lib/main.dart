2edfe672b98a8abc726d85cce5702b41894bc202
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-06-17 02:45:13 +0300

    firebase impl

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quote_generator/common/shared_prefs/shared_prefs.dart';
 import 'app/app.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await SharedPrefs.init();
+  await Future.wait([
+    SharedPrefs.init(),
+    Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    ),
+  ]);
   runApp(
     const ProviderScope(
       child: QuoteGeneratorApp(),
