915dfb13e10c2c1e4dcd72ee903c3a5cf4bfb768
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-06-17 02:33:27 +0300

    Constants private constructor

--- lib/features/quote/presentation/utils/constants.dart
+++ lib/features/quote/presentation/utils/constants.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_animate/flutter_animate.dart';
 
 class Constants {
+  const Constants._();
   static const String dbTable = 'quotes';
   static const String kLocale = 'locale';
   static const String kDarkMode = 'darkMode';
