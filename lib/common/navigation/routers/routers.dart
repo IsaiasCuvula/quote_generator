e841d8e09f8783a69b330c61bb6ac049ce6619a4
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-06-19 02:12:50 +0300

    clean up

--- lib/common/navigation/routers/routers.dart
+++ lib/common/navigation/routers/routers.dart
@@ -1,3 +1,4 @@
 export 'routes_provider.dart';
 export 'routes.dart';
 export 'routes_location.dart';
+export 'error_screen.dart';
--- lib/common/navigation/routers/routes_provider.dart
+++ lib/common/navigation/routers/routes_provider.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:quote_generator/common/common.dart';
-import 'package:quote_generator/common/navigation/routers/error_screen.dart';
 import 'package:quote_generator/features/auth/auth.dart';
 
 final navigationKey = GlobalKey<NavigatorState>();
