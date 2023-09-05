d24b1852166d9125822e19a77f3dbb0202e4374d
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-09-05 22:03:10 +0300

    clean up

--- lib/features/auth/data/datasource/datasource.dart
+++ lib/features/auth/data/datasource/datasource.dart
@@ -1,3 +1,3 @@
-export 'remote/auth_remote_datasource.dart';
-export 'remote/auth_remote_datasource_impl.dart';
-export 'remote/auth_remote_datasource_provider.dart';
+export 'auth_remote_datasource.dart';
+export 'auth_remote_datasource_impl.dart';
+export 'auth_remote_datasource_provider.dart';
